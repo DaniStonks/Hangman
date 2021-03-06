a06d202bf983a408bfaeb65088489c2b0528798f
Rafael <200221065@estudantes.ips.pt>
2021-03-06 23:31:54 +0000

    Menu de iniciou

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,6 +1,42 @@
+import java.sql.SQLOutput;
+
 public class AppStart {
+
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println(" 1 2 3 q134rtwrg");
+        System.out.println(" 1 2 3 ");
+    }
+
+    public void runMenu()
+    {
+        System.out.println("┍──────────────────────────────┑");
+        System.out.println("┃         Menu de jogo         ┃");
+        System.out.println("┃                              ┃");
+        System.out.println("┃ 1- Iniciar o jogo.           ┃");
+        System.out.println("┃ 2- Terminar o jogo.          ┃");
+        System.out.println("┃ 3- Sair do jogo.             ┃");
+        System.out.println("┕──────────────────────────────┙\n");
+
+        /*switch()
+        {
+            case 1:
+                {
+
+                }
+            case 2:
+                {
+
+                }
+            case 3:
+                {
+
+                }
+            default:
+                {
+                    System.out.println("Volte a introduzir a opção:\n");
+                }
+        }
+        */
+
     }
 }
---
+++ src/MainMenu.java
@@ -0,0 +1,9 @@
+public class MainMenu {
+
+    public void runMenu()
+    {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
