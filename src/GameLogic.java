baa45c63ff35fc2a0b1b5705ae08ceba54a1ffa6
Daniel <200221067@estudantes.ips.pt>
2021-03-06 23:30:44 +0000

    Initial GameLogic

---
+++ src/GameLogic.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class GameLogic {
+    private static int lives = 7;
+    private static String[] gameWords = {"nuno" , "pokemon", "pessego", "matematica"
+                                        , "teclado", "netbeans", "baguete", "palavras"
+                                        , "faixas" , "cebolas", "churrasco", "secador"
+                                        , "curgete", "placebo", "paquistano", "aneurisma"};
+    private String gameWord;
+    private char[] receivedLetters;
+}
